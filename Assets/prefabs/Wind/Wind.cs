7a76c3ec2c3c43adae5d71ac52a38c4880ca8129
jvolcy <jerry.volcy@gmail.com>
2022-10-09 04:00:25 -0400

    prior to updated flag animation

---
+++ Assets/prefabs/Wind/Wind.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public int MinSpeed = -30;
+    public int MaxSpeed = +30;
+    public SpriteRenderer DirectionFlag;
+    public TMPro.TMP_Text TxtMPH;
+
+    int speed = 0;
+    public int Speed
+    {
+        set
+        {
+            speed = Mathf.Clamp(value, MinSpeed, MaxSpeed);
+            DirectionFlag.flipX = speed < 0;
+            TxtMPH.text = speed.ToString();
+        }
+
+        get
+        {
+            return speed;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = Random.Range(MinSpeed, MaxSpeed + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //function to simulate the shifting of the wind
+    public void WindSift(int MinDeltaSpeed = -5, int MaxDeltaSpeed = 5)
+    {
+        Speed += Random.Range(-MinDeltaSpeed, MaxDeltaSpeed + 1);
+    }
+}
